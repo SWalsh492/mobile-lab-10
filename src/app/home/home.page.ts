2c79230db5adeb58887e904bef361486efaf3176
SWalsh492 <swalsh818@gmail.com>
2023-04-28 12:04:40 +0100

    Empty first commit.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 stories:any[]=[];
+long:number = 0;
+lat:number = 0;
+time:number = 0;
   constructor() {}
   
+  async getGPS(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+
+    this.time = coordinates.timestamp;
+    this.long = coordinates.coords.longitude;
+    this.lat = coordinates.coords.latitude;
+  }
+
+  async openBrowser() {
+    await Browser.open({ url: 'http://capacitorjs.com/' });
+    };
 }
